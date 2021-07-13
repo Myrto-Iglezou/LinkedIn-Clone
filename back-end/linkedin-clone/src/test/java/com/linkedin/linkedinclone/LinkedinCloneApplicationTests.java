eb0552637a7c5d89f3ff847e6d6060b0de02b604
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2021-07-13 19:29:26 +0300

    Create LinkedinCloneApplicationTests.java

---
+++ back-end/linkedin-clone/src/test/java/com/linkedin/linkedinclone/LinkedinCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.linkedinclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
