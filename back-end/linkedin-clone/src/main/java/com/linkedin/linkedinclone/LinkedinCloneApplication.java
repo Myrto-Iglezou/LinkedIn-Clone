adc7b3558f0a92f99b51c296f2763a332805431a
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2021-07-13 19:29:16 +0300

    new back-end

---
+++ back-end/linkedin-clone/src/main/java/com/linkedin/linkedinclone/LinkedinCloneApplication.java
@@ -0,0 +1,13 @@
+package com.linkedin.linkedinclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedinCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedinCloneApplication.class, args);
+	}
+
+}
