c8aeeab91f798d432016711b1dd650957cdc6de1
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2021-08-08 20:04:23 +0300

    fix library path

--- front-end/src/app/helpers/jwt.interceptor.ts
+++ front-end/src/app/helpers/jwt.interceptor.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {AuthenticationService} from '../../authentication.service';
-import {UserDetails} from '../../model/user-details';
+import {AuthenticationService} from '../authentication.service';
+import {UserDetails} from '../model/user-details';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
