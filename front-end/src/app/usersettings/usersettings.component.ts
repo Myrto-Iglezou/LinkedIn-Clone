e3561d62cd573d6628186e09dcd549294797cfd6
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2021-08-19 19:43:20 +0300

    fix settings forms

--- front-end/src/app/usersettings/usersettings.component.ts
+++ front-end/src/app/usersettings/usersettings.component.ts
@@ -14,13 +14,16 @@ import { UserSettings } from '../model/usersettings';
 export class UsersettingsComponent implements OnInit {
 
   usersettings: UserSettings = new UserSettings();
-  loading = false;
+  loading_1 = false;
+  loading_2 = false;
   dangerBox = false;
   successBox = false;
-  submitattempt = false;
+  submitattempt_1 = false;
+  submitattempt_2 = false;
   submiterror: any ;
   submitmsg: string;
-  currPswdInput: any = {};
+  currPwd_1: string; 
+  currPwd_2: string; 
   userDetails: UserDetails;
 
   constructor(private userService: UserService, private route: ActivatedRoute , private authenticationService: AuthenticationService) { }
@@ -34,18 +37,25 @@ export class UsersettingsComponent implements OnInit {
         this.usersettings.id = user.id;
         this.usersettings.currentPassword = user.password;
         this.usersettings.currentUsername = user.username;
+        this.usersettings.newPassword = null;
+        this.usersettings.passwordConfirm = null;
+        this.usersettings.newUsername = null;
       }
     );
   }
 
 
-  submit(usereditform){
-    if (usereditform.form.valid  && (this.usersettings.newPassword === this.usersettings.passwordConfirm)) {
-      this.loading = true;
+  emailSubmit(userform){
+    if (userform.form.valid) {
+      this.loading_1 = true;
+
+      this.usersettings.newPassword = null;
+      this.usersettings.passwordConfirm = null;
+
       this.userService.editUserSettings(this.usersettings)
         .subscribe(
           response => {
-            this.loading = false;
+            this.loading_1 = false;
             const token = response.headers.get('Authorization');
             if (token) {
               let userDetails: UserDetails = null;
@@ -60,16 +70,52 @@ export class UsersettingsComponent implements OnInit {
             this.dangerBox = false;
           },
           error => {
-            this.loading = false;
+            this.loading_1 = false;
             this.submiterror = error;
             this.dangerBox = true;
             this.successBox = false;
-            this.submitattempt = true;
+            this.submitattempt_1 = true;
+            alert(error.message);
           }
         );
     }
     else{
-      this.submitattempt = true;
+      this.submitattempt_1 = true;
+      this.dangerBox = false;
+    }
+  }
+  pwdSubmit(userform){
+    if (userform.form.valid  && (this.usersettings.newPassword === this.usersettings.passwordConfirm)) {
+      this.loading_2 = true;
+      this.usersettings.newUsername = null;
+      this.userService.editUserSettings(this.usersettings)
+        .subscribe(
+          response => {
+            this.loading_2 = false;
+            const token = response.headers.get('Authorization');
+            if (token) {
+              let userDetails: UserDetails = null;
+              this.authenticationService.getLoggedInUser().subscribe((uDetails) => {
+                  userDetails = uDetails;
+              });
+              userDetails.token = token;
+              this.authenticationService.setLoggedInUser(userDetails);
+            }
+            this.submitmsg = response.body;
+            this.successBox = true;
+            this.dangerBox = false;
+          },
+          error => {
+            this.loading_2 = false;
+            this.submiterror = error;
+            this.dangerBox = true;
+            this.successBox = false;
+            this.submitattempt_2 = true;
+          }
+        );
+    }
+    else{
+      this.submitattempt_2 = true;
       this.dangerBox = false;
     }
 
