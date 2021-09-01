85d6272e72bc9b72de1119fa1d0512ab2d9150c5
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2021-09-01 12:53:09 +0300

    start admin page

---
+++ front-end/src/app/services/admin.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { User } from '../model/user';
+import { Observable } from 'rxjs';
+import {HttpClient, HttpHeaders, HttpResponse} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+
+    return this.http.get<User[]>('https://localhost:8443/admin/users');
+  }
+}
--- front-end/src/app/services/user.service.ts
+++ front-end/src/app/services/user.service.ts
@@ -15,12 +15,7 @@ export class UserService {
 
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<User[]> {
-
-    return this.http.get<User[]>('https://localhost:8443/admin/users');
-  }
-
-  getUser(id: string): Observable<User> {
+   getUser(id: string): Observable<User> { 
     return this.http.get<User>('https://localhost:8443/in/' + id);
   }
 
