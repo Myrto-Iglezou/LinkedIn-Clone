90a023f66f08a45b3369d8b63e82379307465082
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2021-09-01 10:27:39 +0300

    create messaging component

---
+++ front-end/src/app/messaging/messaging.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messaging',
+  templateUrl: './messaging.component.html',
+  styleUrls: ['./messaging.component.css']
+})
+export class MessagingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
