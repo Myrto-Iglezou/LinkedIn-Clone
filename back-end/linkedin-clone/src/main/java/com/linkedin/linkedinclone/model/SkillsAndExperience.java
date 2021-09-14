bbf9cc437e1936dabaab995db86bbeac994e7a77
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2021-09-14 20:22:46 +0300

    Update SkillsAndExperience.java

--- back-end/linkedin-clone/src/main/java/com/linkedin/linkedinclone/model/SkillsAndExperience.java
+++ back-end/linkedin-clone/src/main/java/com/linkedin/linkedinclone/model/SkillsAndExperience.java
@@ -17,7 +17,7 @@ import java.util.Set;
 @NoArgsConstructor
 public class SkillsAndExperience {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated
